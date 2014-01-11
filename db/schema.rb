88f0e1f7b1cde302e1d824a6f57f84e0a551bb24
Tyler Wasden <twasden@gmail.com>
2014-01-10 22:05:00 -0600

    Add need model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111033719) do
+ActiveRecord::Schema.define(version: 20140111035037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "needs", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "offers", force: true do |t|
     t.string   "status"
