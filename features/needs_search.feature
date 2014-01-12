d28f173cb22911a62afe844ae6c263b6cecf6b18
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-01-11 22:56:08 -0600

    added simple search feature test

--- features/needs_search.feature
+++ features/needs_search.feature
@@ -1,6 +1,6 @@
 Feature: Search for needs on C2G home page
 
   Scenario: Search for needs
-    Given I am on the C2G home page
+    Given I am on the "home" page
     When I search for "book"
     Then needs related to books are returned
---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,12 @@
+Given(/^I am on the "(.*?)" page$/) do |page|
+  visit 'http://localhost:3000'
+end
+
+When(/^I search for "(.*?)"$/) do |term|
+  fill_in 'query', with: term
+  click_on 'Help!'
+end
+
+Then(/^needs related to books are returned$/) do
+  page.should have_content 'book'
+end
