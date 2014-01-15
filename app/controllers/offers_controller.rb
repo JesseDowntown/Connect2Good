2bd41a2d8a662ec43f1e0f83de8bb44704a54da8
Tyler Wasden <twasden@gmail.com>
2014-01-15 14:21:40 -0600

    update offer form to be dynamic and create redirect after sign_in/out to previous page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -31,6 +31,7 @@ class OffersController < ApplicationController
     end
 
     if @offer.save
+      Notifier.offer_received(@offer).deliver
       redirect_to @offer, notice: 'Offer was successfully created.'
     else
       render action: 'new'
