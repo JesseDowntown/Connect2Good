757ab6aa84118471563d00a6b52aacb11368fa5d
Jeff Conner <drumwagon@gmail.com>
2014-01-15 13:03:21 -0600

    Update Foundation override files

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -31,7 +31,6 @@ class OffersController < ApplicationController
     end
 
     if @offer.save
-      Notifier.offer_received(@offer).deliver
       redirect_to @offer, notice: 'Offer was successfully created.'
     else
       render action: 'new'
