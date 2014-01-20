741e5e4bb31446b7250724b0535f7f5d662e32ce
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-01-19 20:39:03 -0600

    add tests

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
-describe Offer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+
+# rating = Rating.create(video_id: video.id, user_id: user.id, value: 5)
+# expect(rating).to_not be_valid
+# expect(Rating.count).to eq 1
+# expect(rating.errors.full_messages).to include("Video has already been taken")
