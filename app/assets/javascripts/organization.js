9489e51ecf97f59ea07a2101493a895cd8278121
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-01-21 17:31:35 -0600

    fix jumpy load

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  $('.org-edit .edit_organization').hide()
-
+  
   $('.org-edit .button').on('click', function(e) {
     $('.org-edit .edit_organization').slideToggle();
     $('.org-edit .button').hide();
