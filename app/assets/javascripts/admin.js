b648a06319888fe37ac3a433bffd88c3c4d2a7c9
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-01-22 12:01:56 -0600

    add chart to admin dash

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
     e.preventDefault();
   })
 
+
+
+  //Get context with jQuery - using jQuery's .get() method.
+  var ctx = $("#myChart").get(0).getContext("2d");
+  //This will get the first returned node in the jQuery collection.
+  var myNewChart = new Chart(ctx).Doughnut(data);
+
 })
