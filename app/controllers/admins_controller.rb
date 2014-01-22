99476b60465f042b005d3aafd00db1d1babb8bb1
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-01-22 16:39:34 -0600

    add graph

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,11 +5,26 @@ class AdminsController < ActionController::Base
   def show
     @organizations = Organization.all
     @organization = Organization.new
+    @offers = Offer.all
 
     @active = 0
+    @pending = 0
+    @accepted = 0
+    @denied = 0
     @organizations.each do |organization|
       organization.status == true ? @active += 1 : @active
+      organization.offers.each do |offer|
+        if offer.status == 'pending'
+          @pending += 1
+        elsif offer.status == 'accepted'
+          @accepted += 1
+        else
+          @denied += 1
+        end
+      end
     end
+
+
   end
 
 
