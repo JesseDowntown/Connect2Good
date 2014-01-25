e15d00797b2522aec276cd5407ebb533a7173e88
Sonny Gonzalez <sonnyg.email@gmail.com>
2014-01-25 11:58:02 -0600

    add end to fix syntax error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,4 +9,4 @@ CarrierWave.configure do |config|
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }  # optional, defaults to {}
   config.fog_directory  = ENV['AWS_S3_BUCKET']
-
+end
