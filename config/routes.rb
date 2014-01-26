88210204bccb3ef283854a358e04f5e0d60c2cac
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-01-26 13:17:34 -0600

    switch welcome controller to pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ C2g::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#index'
 
   get 'admin', to: 'admins#show'
 
