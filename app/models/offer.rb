5094412846a52953ccb135dfb56a24210f0d565d
Tyler Wasden <twasden@gmail.com>
2014-01-27 11:21:41 -0600

    require email validation for everyone

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,12 +4,12 @@ class Offer < ActiveRecord::Base
     foreign_key: 'donor_id' 
   belongs_to :need
   belongs_to :organization
-  validates :email, presence: true, :if => :signed_in?
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :if => :signed_in?
+  validates :email, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
  
   mount_uploader :image, ImageUploader
  
-  def signed_in?
-    donor_id.present? == false 
-  end
+  # def signed_in?
+  #   donor_id.present? == false 
+  # end
 end
